e93f836924edd6b199a9bd395026788aa4cc7090
Johnny Dockhorn <johnnyerick111@hotmail.com>
2021-01-20 17:02:52 -0200

    Add project files.

---
+++ src/DevIO.Business/Notificacoes/Notificacao.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace DevIO.Business.Notificacoes
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get;}
+
+    }
+}
